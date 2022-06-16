0edbfe15326a8c69d99cfb2a5593b440bb10daf3
Aleksandra Ujvari <ujvari65@gmail.com>
2022-06-16 08:44:25 +0200

    Fix linters.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
+const openPopupButtons = document.querySelectorAll('[data-popup-target]');
+const closePopupButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
@@ -10,3 +13,4 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
