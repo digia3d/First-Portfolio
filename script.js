97cdb0b6152587fc67dc605be9a3809645a9ad7e
Aleksandra Ujvari <ujvari65@gmail.com>
2022-06-16 14:40:17 +0200

    Add JS for button.

--- script.js
+++ script.js
@@ -2,29 +2,30 @@ const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 const projectDetailsButtons = document.querySelectorAll('.detailsButton');
 const popup = document.querySelector('#popup');
+const closePopupButtons = querySelectorAll('#closeButton');
 const projects = {
   1: {
-    name: 'First',
+    name: 'Multi-Post Stories',
     imageSrc: 'img/popup/Snapshoot-Portfolio.png',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting ever since the 1500s, when an unknown printer took a galley of type  veris lapoa todoe.',
     languages: ['html', 'Ruby on rails', 'css'],
   },
   2: {
-    name: 'Second',
-    imageSrc: '',
-    description: '',
+    name: 'Multi-Post Stories',
+    imageSrc: 'img/popup/Snapshoot-Portfolio.png',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting ever since the 1500s, when an unknown printer took a galley of type  veris lapoa todoe.',
     languages: ['html', 'php', 'css'],
   },
   3: {
-    name: 'Third',
-    imageSrc: '',
-    description: '',
+    name: 'Multi-Post Stories',
+    imageSrc: 'img/popup/Snapshoot-Portfolio.png',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting ever since the 1500s, when an unknown printer took a galley of type  veris lapoa todoe.',
     languages: ['html', 'javascript', 'css'],
   },
   4: {
-    name: 'Fourth',
-    imageSrc: '',
-    description: '',
+    name: 'Multi-Post Stories',
+    imageSrc: 'img/popup/Snapshoot-Portfolio.png',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting ever since the 1500s, when an unknown printer took a galley of type  veris lapoa todoe.',
     languages: ['html', 'java', 'css'],
   },
 };
@@ -79,5 +80,13 @@ projectDetailsButtons.forEach((btn) => {
     setPopupData(projects[ev.target.attributes.id.value]);
     document.querySelector('header').classList.add('blur');
     popup.classList.remove('d-none');
+  });
+});
+
+closePopupButtons.forEach((btn) => {
+  btn.addEventListener('click', (ev) => {
+    setPopupData(projects[ev.target.attributes.id.value]);
+    document.querySelector('header').classList.remove('blur');
+    popup.classList.add('d-none');
   });
 });
