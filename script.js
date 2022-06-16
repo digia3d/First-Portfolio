709bd4e087eecd9d1f3a1baf16d0b4617b68d964
Aleksandra Ujvari <ujvari65@gmail.com>
2022-06-16 19:37:39 +0200

    Fix error that reviewer sugested.

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ const projects = {
   },
   2: {
     name: 'Multi - Post stories',
-    imageSrc: 'img/popup/Snapshoot-Portfolio.png',
+    imageSrc: '',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting ever since the 1500s, when an unknown printer took a galley of type  veris lapoa todoe.',
     languages: ['html', 'php', 'css'],
   },
   3: {
     name: 'Multi - Post stories',
-    imageSrc: 'img/popup/Snapshoot-Portfolio.png',
+    imageSrc: '',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting ever since the 1500s, when an unknown printer took a galley of type  veris lapoa todoe.',
     languages: ['html', 'javascript', 'css'],
   },
   4: {
     name: 'Multi - Post stories',
-    imageSrc: 'img/popup/Snapshoot-Portfolio.png',
+    imageSrc: '',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting ever since the 1500s, when an unknown printer took a galley of type  veris lapoa todoe.',
     languages: ['html', 'java', 'css'],
   },
@@ -80,7 +80,7 @@ projectDetailsButtons.forEach((btn) => {
     setPopupData(projects[ev.target.attributes.id.value]);
     document.querySelector('header').classList.add('blur');
     document.querySelectorAll('section').forEach((item) => item.classList.add('blur'));
-    popup.classList.remove(' d-none ');
+    popup.classList.remove('d-none');
     window.scrollTo(0, 0);
   });
 });
