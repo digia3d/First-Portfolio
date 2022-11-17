ccd4c4d2ad8d9d5cc7cb544fbb9d273106f52ffb
Aleksandra Ujvari <ujvari65@gmail.com>
2022-11-17 19:16:10 +0100

    Add new project

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ const projects = {
     languages: ['html', 'Ruby on rails', 'css'],
   },
   2: {
-    name: 'Multi - Post stories',
-    imageSrc: 'img/TO_DO.png',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting ever since the 1500s, when an unknown printer took a galley of type  veris lapoa todoe.',
-    languages: ['html', 'php', 'css'],
+    name: 'FAMILY WALLET',
+    imageSrc: 'img/Expenses-Category page.png',
+    description: 'A mobile web application where you can manage your budget.',
+    languages: ['html', 'Ruby on rails', 'css'],
   },
   3: {
     name: 'Multi - Post stories',
